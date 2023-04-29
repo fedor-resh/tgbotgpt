9800bedb5cd90d870ba2616f0c00e794897991f6
fedor-resh <87930362+fedor-resh@users.noreply.github.com>
2023-04-29 07:50:07 +0000

    fix session clearing

--- src/logic.js
+++ src/logic.js
@@ -6,6 +6,7 @@ export const INITIAL_SESSION = () => ({
 
 export async function initCommand(ctx) {
   const userId = ctx.from.id
+  ctx.session ??= {}
   ctx.session[userId] = INITIAL_SESSION()
   await ctx.reply('Жду вашего голосового или текстового сообщения')
 }
