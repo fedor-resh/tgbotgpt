12e8738ae3bf5adb65111a2f86354be9aad7d468
fedor-resh <87930362+fedor-resh@users.noreply.github.com>
2023-05-11 17:11:26 +0000

    refactor waiter

--- src/waiter.js
+++ src/waiter.js
@@ -4,34 +4,34 @@ export class Waiter {
     interval = null
     msg = null
     ctx = null
-    text_to_wait = 'Loading.'
+    text_to_wait = 'Loading'
     async start(ctx) {
         this.msg = await ctx.reply(code(this.text_to_wait))
+      this.ctx = ctx
         let dotsCount = 0
         this.interval = setInterval(async () => {
             dotsCount += 1
-            const dots = '.'.repeat(dotsCount % 3)
-            await ctx.telegram.editMessageText(
-                this.msg.chat.id,
-                this.msg.message_id,
-                null,
-                code(this.text_to_wait + dots)
-            )
+            edit(dotsCount)
         }, 500)
-        this.ctx = ctx
+        
+    }
+
+    async edit(dotsCount){
+      const dots = '.'.repeat(dotsCount % 4)
+      try {
+        await ctx.telegram.editMessageText(
+          this.msg.chat.id,
+          this.msg.message_id,
+          null,
+          code(this.text_to_wait + dots)
+        )
+      } catch (error) {}
     }
 
     async stop() {
         clearInterval(this.interval)
         setTimeout(async () => {
-            try{
-                await this.ctx.telegram.editMessageText(
-                    this.msg.chat.id,
-                    this.msg.message_id,
-                    null,
-                    code(this.text_to_wait + '...')
-                )
-            }catch (e) {}
+          edit(3)
         }, 100)
     }
 }
